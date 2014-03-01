3b7140c183196b250aabaeb97faf3f9969497dac
Aaron Thode <athode@ucsd.edu>
2014-02-28 16:05:01 -0800

    cleaned up load_program_location

--- source/deps/load_program_location.m
+++ source/deps/load_program_location.m
@@ -16,24 +16,6 @@ elseif strcmp(getenv('USER'),'fielduser')
 elseif ~isempty(findstr(local_machine,'thode'))
     program_dir ='/Users/thode/Documents/workspace/DataPreProcessor/PreProcessV1.jar';
     
-elseif ~isempty(findstr(local_machine,'thode'))
-    program_dir ='/Users/thode/Documents/workspace/DataPreProcessor/PreProcessV1.jar';
-elseif ~isempty(findstr(local_machine,'dgrebner'))
-    program_dir ='/Users/dgrebner/Projects/DataPreProcessor/PreProcessV1.jar';
-elseif ~isempty(findstr(local_machine,'melania'))
-    program_dir ='/Users/melania/Projects/Matlab_IO/DataPreProcessor/PreProcessV1.jar';
-elseif ~isempty(findstr(local_machine,'delphine'))
-    %program_dir ='/Users/delphine/Delphine/DataPreProcessor/PreProcessV1.jar'; 
-    program_dir='/Users/delphine/Delphine/MATLAB_SVN/DataPreProcessor/PreProcessV1.jar'; 
-
-elseif strcmp(getenv('USER'),'dponcemorado')
-    program_dir='~/Desktop/Graywhale_Part2/ThodeLabSoftware.dir/JAVA_programs/DataPreProcessor/PreProcessV1.jar';
-elseif ~isempty(findstr(local_machine,'Janice'))|~isempty(findstr(local_machine,'alaska.edu'))
-    program_dir='~/Desktop/DataPreProcessor/PreProcessV1.jar';
-elseif ~isempty(findstr(local_machine,'uas.ad'))|~isempty(findstr(local_machine,'raptor'))
-    program_dir='~/Desktop/DataPreProcessor/PreProcessV1.jar';
-elseif ~isempty(findstr(local_machine,'ucsd.edu'))
-    program_dir ='/Users/dponcemorado/Desktop/ThodeLabSoftware.dir/JAVA_programs/DataPreProcessor/PreProcessV1.jar';
 else
     program_dir='~/Desktop/DataPreProcessor/PreProcessV1.jar';
     if exist(program_dir,'file')~=2
