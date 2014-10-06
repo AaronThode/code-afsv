62ce53798d90d6f98a2a294266e15b4a81228045
Aaron Thode <athode@ucsd.edu>
2014-10-06 12:22:45 -0700

    Beamforming on SIO (debugging) and debugging annotations using inputsdlg, and CSDM pushbutton now allows a slice of time to be selected...

--- source/AllFile_specgram_viewer.m
+++ source/AllFile_specgram_viewer.m
@@ -3557,7 +3557,7 @@ tlen=handles.tlen;
 mydir=pwd;
 Ichan='all';  %Hardwire first channel
 set(handles.togglebutton_ChannelBeam,'String','Channel');
-[x,~,Fs,~,~,head]=load_data(handles.filetype, tdate_start,tlen,Ichan,handles);
+[x,t,Fs,~,~,head]=load_data(handles.filetype, tdate_start,tlen,Ichan,handles);
 
 %If not multichannel data, return
 if isempty(x)||~head.multichannel
@@ -3625,8 +3625,14 @@ if Ichc==1
     hold off
 elseif Ichc==2  %extractKsexact
     %%process all frequencies over a given range
-    disp('Select two frequencies from image:')
+    disp('Select bounding box for time and range:');
     ftmp=ginput(2);
+    
+    %Trim x...
+    Igood=( t>=min(ftmp(:,1))&t<=max(ftmp(:,1)));
+    x=x(Igood,:);
+    
+    
     frange=sort(ftmp(:,2)*1000);
     fprintf('frange: %6.2f to %6.2f Hz\n',frange);
     threshold=input('Enter threshold in dB[-Inf]:');
@@ -3844,9 +3850,9 @@ end
         end
         
         df=Ksout.freq(2)-Ksout.freq(1);
-        plot(angles,sum(10*log10((B)))/length(Ksout.freq),'k');
-        set(gca,'fontweight','bold','fontsize',14);
-        ylabel('Mean dB Beampower ');xlabel('Angle from horizontal (deg)');grid on;
+        plot(sum(10*log10((B)))/length(Ksout.freq),angles,'k');
+        set(gca,'fontweight','bold','fontsize',14);axis('ij');
+        xlabel('Mean dB Beampower ');ylabel('Angle from horizontal (deg)');grid on;
         title(sprintf('%s, %i FFT, %i elements',datestr(tdate_start,'dd-mmm-yyyy HH:MM:SS.FFF'),Nfft,length(head.geom.rd)));
         
         if yes==4
@@ -8315,7 +8321,10 @@ switch filetype
                 disp('load_data: sioread beamform failure');
                 keyboard
             end
-         end
+        end
+         
+        %Set time
+        t=(1:size(x,1))/Fs;
         
         
     case 'DAT'
@@ -9122,173 +9131,6 @@ Ksout.SN=SN;
 close;
 end
 
-function [Kstot,f,VV,EE_sort]=extractKsexact(x,ovlap,Nfft,chann,frange,Fs,Isnap,M,nowin,threshold,tiltdata)
-
-%%%%%%%%%%%%%%%%%%%%%%%extractKsexact.m%%%%%%%%%%%
-%function [Kstot,Ks_eig,Ns,f,power]=extractKsexact(x,ovlap,Nfft,chann,frange,Fs,Isnap,M,nowin);
-%[Kstot,pgoal,Ns,f,pwr_est,SNRest,pphase]=extractKsexact(x,ovlap,Nfft,chann,frange,Fs,Isnap,M,nowin);
-%  Aaron Thode
-%  July 3, 1996
-%  Generates averaged cross-spectral outputs
-%    from data input
-%  CRUCIAL:  There is a line that flips pgoal so that the first
-%		element represents the top phone.
-%x=array of data, rows are time, columns are channels
-%ovlap=overlap of time samples
-%Nfft-number of point desired taken
-%chann-vector containing element number to be used:
-%frange-pairs of frequencies that define desired ranges If odd, last
-%	element is bin spacing (i.e.) evaluate every other bin
-%Isnap %Select the Isnap window.  If negative, average
-%M amount of dataused for the fft snapshot
-% nowin-if exists, don't window the data before using fft.used for source signature estimates
-% threshold-dB threshold of power (sum of energy across all frequencies and channels) to reject a snapshot.
-%    Set to Inf to ensure all snapshots used.
-% tiltdata: tilt: estimated vertical tilt of array in degrees.
-%           rd: element depths in m
-% Output: Kstot-CSDM
-%          %V: eigenvectors of CSDM
-%          %ev: eigenvalues of CSDM
-%  April 1, 2004: normalize CSDM to have units of power spectral density:
-%  |X(f)|^2/(Fs*Nfft), Power/Hz
-VV=[];
-EE_sort=[];
-MAXF=Inf;
-if ~exist('threshold','var')
-    threshold = -Inf;
-end
-
-if ~exist('tilt','var')
-    tiltdata.tilt=0; %
-    tiltdata.rd=ones(length(chann),1);
-end
-if size(tiltdata.rd,2)>1
-    tiltdata.rd=tiltdata.rd';
-end
-sintilt=sin(tiltdata.tilt*pi/180);
-if ~exist('nowin','var')
-    nowin=0;
-    disp('The signal will be windowed');
-elseif nowin==1,
-    nowin=1;
-    disp('The signal will NOT be windowed');
-else
-    nowin=0;
-    disp('The signal will be windowed');
-end
-
-Nel=length(chann);
-Ns=floor(((size(x,1)/M)-ovlap)/(1-ovlap));
-disp(Ns);
-%pause;
-%Select appropriate frequency bin
-%[f,findex]=makefaxis(Nfft,Fs,frange);
-frange=round((Nfft/Fs)*frange);
-findex=max([1 frange(1)]):min([Nfft/2 frange(2)]);
-f=findex*(Fs/Nfft);
-
-if length(findex)<MAXF
-    Kstot=zeros(Nel,Nel,length(findex));
-else
-    disp('frange too long to make Kstot\n just making pgoal');
-end
-power=zeros(Ns,1);
-Nf=length(findex);
-if	~exist('Isnap', 'var')|| Isnap<0
-    Isnap=0:Ns-1; %average all
-end
-
-if Ns<=0,
-    disp('Signal too short for one shapnot, will center pad for FFT:');
-    %pause;
-    Ns=1;Isnap=0;Nx=size(x,1);
-    x0=zeros(Nfft,size(x,2));
-    index=floor(Nfft/2-(Nx/2));
-    index=(index:(index+Nx-1));
-    x0(index,:)=x;
-    x=x0;
-    clear x0
-    M=size(x,1);
-end
-if nowin==0,
-    win=kaiser(M,2.5);
-else
-    win=ones(M,1);
-end
-
-for I=Isnap
-    index=round(I*M*(1-ovlap)+1);
-    xindex=(index:(index+M-1));
-    xh=x(xindex,chann);
-    for Ic=1:length(chann)
-        xh(:,Ic)=xh(:,Ic)-mean(xh(:,Ic));
-        xh(:,Ic)=xh(:,Ic).*win;
-    end
-    Xh=fft(xh,Nfft);
-    pgoal=Xh(findex,:);
-    %Make pgoal a vertical array
-    %pgoal=pgoal(:,chann);   %Reject elements ;
-    %pgoal=conj(pgoal);   %Test to see if conjugation is the problem
-    pgoal=pgoal.';        %Columns are now single-frequency array snapshots
-    %pgoal=flipud(pgoal);  %Puts topmost element first, according to lewis
-    
-    
-    power(I+1)=(Fs/Nfft)*sum(sum(abs(pgoal).^2))/(Fs*Nfft);
-    
-    if length(findex)<MAXF&&threshold<=10*log10(abs(power(I+1)))
-        for J=1:Nf
-            %disp(f(J));
-            
-            tiltvec=exp(1i*2*pi*f(J)*sintilt*tiltdata.rd/1500);
-            ptemp=pgoal(:,J).*tiltvec;
-            Kstemp=ptemp*ptemp'; %Top LH cornertop element autocor
-            Kstot(:,:,J)=Kstot(:,:,J)+Kstemp;
-            
-        end
-    else
-        power(I+1)=NaN;
-    end
-end  %I=Isnap
-
-figure
-tt=Isnap*(1-ovlap)*Nfft/Fs;
-plot(tt,10*log10(abs(power)));
-grid on
-if ~isinf(threshold)
-    hold on
-    line([min(tt) max(tt)],threshold*[1 1]);
-end
-xlabel('Time (s)');ylabel('dB power');
-
-if length(findex)>MAXF,
-    Kstot=[];
-end
-
-%Normalize to have units of power spectral density...pwr per Hz
-Kstot=Kstot/(Fs*Nfft);
-
-%Compute eigenvalues of CSDM, and the ratio of the high-power to the
-%low-power sound be like SNR
-
-
-%SNR=zeros(1,Nf);
-if Ns>1
-    EE_sort=zeros(Nel,Nf);
-    VV=zeros(Nel,Nel,Nf);
-    for If=1:Nf
-        [VV0,EE]=eig(Kstot(:,:,If));
-        EE=diag(EE);
-        [EE_sort(:,If),Isort]=sort(EE,1,'descend');
-        VV(:,:,If)=VV0(:,Isort);
-        
-    end
-end
-
-% if Ns<=1
-%     SNRest=-SNRest;
-% end
-
-end
 
 function [B,wout]=conventional_beamforming(Ks,angles,freq,Lz,c,yesnorm)
 B=zeros(length(freq),length(angles));
---
+++ source/deps/simulated_plane_wave_arrival.m
@@ -0,0 +1,101 @@
+%%%simulate_plane_wave_arrival.m%%%%%
+% Simulate a signal arriving across a set of array elements
+
+%function x=simulated_plane_wave_arrival(params,Iplot)
+%Input:
+%    params: structure with fields:
+%           signal_type: 'impulse','FM'
+%           c:  medium sound speed in m/s
+%           Nchan: number of channels
+%           angle: degrees, '0' is broadside
+%           L:  array spacing in meters
+%           Fs: sampling rate in Hz
+%           total_window_time:  total sample duration in seconds
+%           delay: relative delay of signal relative to center of signal,
+%                   sec (must be positive number)
+%           noise_var:  how much noise to add: variance relative to a peak
+%               of '1'
+%           FM: structure with FM parameters
+%               fstart=100;
+%               fend=300;
+%               tduration=1;
+%               tstart=1;
+%               SNRdB=8;
+%               SNRchc='PSD';
+function [x,t]=simulated_plane_wave_arrival(params,Iplot)
+
+signal_type=params.signal_type;  %'impulse','FM'
+Nchan=params.Nchan;
+angle=params.angle;
+delay0=ceil(params.Fs*params.delay);
+L=params.L;  %Array spacing in meters
+%params.Fs=4000;
+total_window_time=params.total_window_time;
+
+switch signal_type
+    case 'FM'
+        fstart=params.FM.fstart;
+        fend=params.FM.fend;
+        tduration=params.FM.tduration;
+        tstart=params.FM.tstart;
+        SNRdB=params.FM.SNRdB;
+        SNRchc=params.FM.SNRchc;
+        
+        disp('FM Sweep');
+        [y,ysignal,ynoise,noise_var]=simulate_FMsweep_with_noise(params.Fs,fstart,fend, ...
+            total_window_time,tduration,tstart,SNRdB,SNRchc);
+    case 'impulse'
+        total_window_time=256/params.Fs;
+        
+        Npt=round(total_window_time*params.Fs);
+        y=zeros(Npt,1);
+        y(Npt/2,1)=1;
+        y(Npt/2-1,1)=0.5;
+        y(Npt/2+1,1)=0.5;
+        y(Npt/2-2,1)=0.25;
+        y(Npt/2+2,1)=0.25;
+        
+        if isfield(params,'noise_var')
+            noise_var=params.noise_var;
+        else
+            noise_var=0.001;
+        end
+        ysignal=y;
+        
+end
+
+
+delay=(params.Fs*sin(angle*pi/180)*L/params.c);
+x=ones(1+(Nchan+1)*ceil(max(abs(delay)))+ceil(max(abs(delay0)))+length(y),Nchan);
+x=sqrt(noise_var)*randn(size(x)).*x;
+for J=1:length(angle)
+    for I=1:Nchan
+        if delay(J)>0
+            K=round(delay0(J)+(I-1)*delay(J))+1+(1:length(y));
+            x(K,I)=x(K,I)+ysignal;
+        else
+            K=round(delay0(J)+(Nchan-I)*abs(delay(J)))+1+(1:length(y));
+            
+            x(K,I)=x(K,I)+ysignal;
+            
+        end
+        
+        
+    end
+end
+
+t=(1:length(x(:,1)))/params.Fs;
+if Iplot>0
+    figure
+    
+    for I=1:Nchan
+        subplot(Nchan,1,I)
+        plot(t*1000,x(:,I))
+    end
+    xlabel('Time (msec)');
+end
+
+%
+%end
+end
+
